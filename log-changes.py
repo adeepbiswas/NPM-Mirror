eb30095c60ecc52acca59a5546b1218dce16a642
Adeep Biswas <adeepb@andrew.cmu.edu>
2023-08-03 10:49:30 -0400

    added local/remote download separation, size limit

--- log-changes.py
+++ log-changes.py
@@ -4,8 +4,28 @@ import json
 import tarfile
 import zipfile
 import re
+import couchdb
+import datetime
 
-LOCAL_PACKAGE_DIR = "packages"
+LOCAL_PACKAGE_DIR = "temp_packages"
+REMOTE_PACKAGE_DIR = "packages"
+max_size = 5e+6
+db_user = 'admin'
+db_password = 'opensesame123'
+database_name = 'new-toy-db'
+
+# Establish server connection
+server = couchdb.Server('http://{user}:{password}@localhost:5984/'.format(user=db_user, password=db_password))
+
+# Function to create or connect to the CouchDB database
+def create_or_connect_db(server, database_name):
+    try:
+        db = server.create(database_name)
+        print(f"Created new database: {database_name}")
+    except couchdb.http.PreconditionFailed:
+        db = server[database_name]
+        print(f"Connected to existing database: {database_name}")
+    return db
 
 def remove_special_characters(input_string):
     # Define a regex pattern to match all non-alphanumeric characters except "/"
@@ -18,6 +38,9 @@ def remove_special_characters(input_string):
 
 def create_directory_structure(package_name):
     # Create parent directory for all packages if it doesn't exist
+    if not os.path.exists(REMOTE_PACKAGE_DIR):
+        os.mkdir(REMOTE_PACKAGE_DIR)
+        
     if not os.path.exists(LOCAL_PACKAGE_DIR):
         os.mkdir(LOCAL_PACKAGE_DIR)
 
@@ -25,7 +48,7 @@ def create_directory_structure(package_name):
     first_char = package_name[0].upper()
     # if not first_char.isalpha():
     #     first_char = "NonAlphabetic"
-    alpha_dir = os.path.join(LOCAL_PACKAGE_DIR, first_char)
+    alpha_dir = os.path.join(REMOTE_PACKAGE_DIR, first_char)
     if not os.path.exists(alpha_dir):
         os.mkdir(alpha_dir)
 
@@ -52,7 +75,12 @@ def download_document_and_package(change):
         package_name = change.get('id')
         if package_name:
             # package_name = remove_special_characters(package_name)
-            package_dir = create_directory_structure(package_name)
+            # package_dir = create_directory_structure(package_name)
+            # print(package_dir)
+            package_dir = LOCAL_PACKAGE_DIR
+            if not os.path.exists(LOCAL_PACKAGE_DIR):
+                os.mkdir(LOCAL_PACKAGE_DIR)
+            saved = True
 
             # Save the document as a JSON file
             if "/" in package_name:
@@ -64,29 +92,41 @@ def download_document_and_package(change):
             doc_path = os.path.join(package_dir, doc_filename)
             with open(doc_path, 'w') as doc_file:
                 json.dump(doc, doc_file)
+                print("saved json")
+                
+            if os.path.getsize(doc_path) > max_size:
+                os.remove(doc_path)
+                print("json too large, not saved")
+                saved = False
 
-            # Save the updated package as a tar file
-            # print("----------")
-            # print(package_name)
-            # print(doc.keys())
-            latest = doc['dist-tags']['latest']
-            package_url = doc['versions'][latest]['dist']['tarball']
-            # print("----------")
-            # package_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-{doc['dist-tags']['latest']}.tgz"
-            package_filename = f"{name}_package.tgz"
-            package_path = os.path.join(package_dir, package_filename)
-            response = requests.get(package_url)
-            print("Response - ",response)
-            if response.status_code == 200:
-                with open(package_path, 'wb') as package_file:
-                    package_file.write(response.content)
-                print("saved")
-            return doc_path, package_path
+            if saved:
+                # Save the updated package as a tar file
+                latest = doc['dist-tags']['latest']
+                package_url = doc['versions'][latest]['dist']['tarball']
+                # package_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-{doc['dist-tags']['latest']}.tgz"
+                package_filename = f"{name}_package.tgz"
+                package_path = os.path.join(package_dir, package_filename)
+                response = requests.get(package_url)
+                if response.status_code == 200:
+                    with open(package_path, 'wb') as package_file:
+                        package_file.write(response.content)
+                    print("saved tar file")
+                    
+                    if os.path.getsize(package_path) > max_size:
+                        os.remove(package_path)
+                        print("tar too large, not saved")
+                        os.remove(doc_path)
+                        print("corresponding json not saved as well")
+                        saved = False
+            if saved:
+                return doc_path, package_path
     return None, None
 
 def compress_files(package_name, revision_id, doc_path, package_path):
+    package_dir = create_directory_structure(package_name)
     compressed_filename = f"{package_name}_{revision_id}.zip"
-    with zipfile.ZipFile(compressed_filename, 'w') as zip_file:
+    zip_path = os.path.join(package_dir, compressed_filename)
+    with zipfile.ZipFile(zip_path, 'w') as zip_file:
         if doc_path:
             zip_file.write(doc_path, os.path.basename(doc_path))
             os.remove(doc_path)  # Remove the individual JSON file after compression
@@ -94,24 +134,38 @@ def compress_files(package_name, revision_id, doc_path, package_path):
             zip_file.write(package_path, os.path.basename(package_path))
             os.remove(package_path)  # Remove the individual tar file after compression
 
-def store_change_details(change, compressed_filename, change_count):
+def store_change_details(change, db):
     # Store the important details regarding the change in the local CouchDB database.
     # Replace the following with your code to interact with the local CouchDB.
 
-    change_id = change['id']
-    timestamp = change['timestamp']
-    package_name = change['doc']['name']
-    change_type = change.get('type')
-    # Add any other relevant details you want to store in the database.
+    package_name = change['id']
+    change_seq_id = change['seq']
+    package_revision_id = change['doc']['_rev']
+    package_latest_version = change['doc']['dist-tags']['latest']
+    package_versions_count = len(change['doc']['versions'].keys())
+    package_latest_change_time = change['doc']['time'][package_latest_version]
+    
+    data = {
+        'package_name': package_name, 
+        'change_seq_id': change_seq_id,
+        'package_revision_id': package_revision_id,
+        'package_latest_version': package_latest_version,
+        'package_versions_count': package_versions_count,
+        'package_latest_change_time': package_latest_change_time
+    }
+    db.save(data)
 
 def stream_npm_updates():
-    url = 'https://replicate.npmjs.com/_changes?include_docs=true&feed=continuous&heartbeat=10000&style=all_docs&conflicts=true&since=25318023' #&limit=20'
+    url = 'https://replicate.npmjs.com/_changes?include_docs=true&feed=continuous&heartbeat=10000&style=all_docs&conflicts=true&since=25318031' #&limit=20'
 
     response = requests.get(url, stream=True)
 
     if response.status_code != 200:
         print(f'Error connecting to the CouchDB stream: {response.status_code}')
         return
+
+    # Create or connect to the database
+    db = create_or_connect_db(server, database_name)
 
     # print(response)
     i = 0
@@ -122,14 +176,8 @@ def stream_npm_updates():
         # print(line)
         if line:
             change = json.loads(line)
-            print(change['seq'])
-            print(change['id'])
-            print(change['changes'])
-            print(change['doc']['_rev'])
-            print(change.keys())
             doc_path, package_path = download_document_and_package(change)
-            print(doc_path)
-            if doc_path or package_path:
+            if doc_path:
                 package_name = change['id']
                 if "/" in package_name:
                     segments = package_name.split("/")
@@ -143,6 +191,7 @@ def stream_npm_updates():
                     # Update the change count for the package and get the updated count
                     # change_count = update_change_count(package_name)
                     # Store important details regarding the change in the local CouchDB
-                    # store_change_details(change, f"{change['id']}_{change['timestamp']}.zip", change_count)
+                    store_change_details(change, db)
+        break
 
 stream_npm_updates()

e000e2f937e22fbdcaeebec3ef7ea06d55c1b163
Adeep Biswas <adeepb@andrew.cmu.edu>
2023-08-03 12:26:00 -0400

    cleaned and refactored code

--- log-changes.py
+++ log-changes.py
@@ -9,15 +9,15 @@ import datetime
 
 LOCAL_PACKAGE_DIR = "temp_packages"
 REMOTE_PACKAGE_DIR = "packages"
-max_size = 5e+6
-db_user = 'admin'
-db_password = 'opensesame123'
-database_name = 'new-toy-db'
+MAX_SIZE = 5e+6
+DB_USER = 'admin'
+DB_PASSWORD = 'opensesame123'
+DATABASE_NAME = 'new-toy-db'
 
-# Establish server connection
-server = couchdb.Server('http://{user}:{password}@localhost:5984/'.format(user=db_user, password=db_password))
+# Establish Couchdb server connection
+server = couchdb.Server('http://{user}:{password}@localhost:5984/'.format(user=DB_USER, password=DB_PASSWORD))
 
-# Function to create or connect to the CouchDB database
+# Function to create or connect to our own CouchDB database
 def create_or_connect_db(server, database_name):
     try:
         db = server.create(database_name)
@@ -27,27 +27,22 @@ def create_or_connect_db(server, database_name):
         print(f"Connected to existing database: {database_name}")
     return db
 
+# Function to remove special characters from package names
 def remove_special_characters(input_string):
-    # Define a regex pattern to match all non-alphanumeric characters except "/"
     pattern = r"[^a-zA-Z0-9/]"
-    
-    # Use re.sub() to replace all matched characters with an empty string
     cleaned_string = re.sub(pattern, "", input_string)
-    # print("cleam string - ", cleaned_string)
     return cleaned_string
 
+# Function to create temporary local directory and organized remote (NAS) directory structure
 def create_directory_structure(package_name):
-    # Create parent directory for all packages if it doesn't exist
+    #Create parent directory in local and remote storage system
     if not os.path.exists(REMOTE_PACKAGE_DIR):
         os.mkdir(REMOTE_PACKAGE_DIR)
-        
     if not os.path.exists(LOCAL_PACKAGE_DIR):
         os.mkdir(LOCAL_PACKAGE_DIR)
 
-    # Create subdirectories for each alphabet (A-Z)
+    # Create subdirectories based on first character of the package name to add heirarchy
     first_char = package_name[0].upper()
-    # if not first_char.isalpha():
-    #     first_char = "NonAlphabetic"
     alpha_dir = os.path.join(REMOTE_PACKAGE_DIR, first_char)
     if not os.path.exists(alpha_dir):
         os.mkdir(alpha_dir)
@@ -62,81 +57,83 @@ def create_directory_structure(package_name):
                 os.mkdir(package_dir)
     else:
         package_dir = os.path.join(alpha_dir, package_name)
-        print("Package Name", package_name)
         if not os.path.exists(package_dir):
             os.mkdir(package_dir)
 
     return package_dir
 
-def download_document_and_package(change):
-    doc = change.get('doc') #storing only doc part currently
+# Function to download package JSON and tarball corresponding to each change
+def download_document_and_package(change, package_name):
+    doc = change.get('doc')
     if doc:
-        # package_name = doc.get('_id')
-        package_name = change.get('id')
-        if package_name:
-            # package_name = remove_special_characters(package_name)
-            # package_dir = create_directory_structure(package_name)
-            # print(package_dir)
-            package_dir = LOCAL_PACKAGE_DIR
-            if not os.path.exists(LOCAL_PACKAGE_DIR):
-                os.mkdir(LOCAL_PACKAGE_DIR)
-            saved = True
+        # creating directory in local storage for temporary downlaod and compression 
+        # before moving to remote directory (for faster transfers)
+        package_dir = LOCAL_PACKAGE_DIR
+        if not os.path.exists(LOCAL_PACKAGE_DIR):
+            os.mkdir(LOCAL_PACKAGE_DIR)
+        
+        saved = True
 
-            # Save the document as a JSON file
-            if "/" in package_name:
-                segments = package_name.split("/")
-                name = segments[-1]
-            else:
-                name = package_name
-            doc_filename = f"{name}_doc.json"
-            doc_path = os.path.join(package_dir, doc_filename)
-            with open(doc_path, 'w') as doc_file:
-                json.dump(doc, doc_file)
-                print("saved json")
+        # Save the document as a JSON file in temporary local directory
+        doc_filename = f"{package_name}_doc.json"
+        doc_path = os.path.join(package_dir, doc_filename)
+        
+        with open(doc_path, 'w') as doc_file:
+            json.dump(doc, doc_file)
+            print("Saved package JSON")  
+        if os.path.getsize(doc_path) > MAX_SIZE:
+            os.remove(doc_path)
+            print("Package JSON too large, removed")
+            saved = False
+            doc_path = None
+
+        # Saving the tarball only if the JSON was saved
+        if saved:
+            # Save the updated (latest) package as a tar file in temporary local directory
+            latest = doc['dist-tags']['latest']
+            tarball_url = doc['versions'][latest]['dist']['tarball']
+            # tarball_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-{doc['dist-tags']['latest']}.tgz"
+            tarball_filename = f"{package_name}_package.tgz"
+            tarball_path = os.path.join(package_dir, tarball_filename)
+            
+            response = requests.get(tarball_url)
+            if response.status_code == 200:
+                with open(tarball_path, 'wb') as tarball_file:
+                    tarball_file.write(response.content)
+                print("Saved Tar file")
                 
-            if os.path.getsize(doc_path) > max_size:
-                os.remove(doc_path)
-                print("json too large, not saved")
-                saved = False
-
-            if saved:
-                # Save the updated package as a tar file
-                latest = doc['dist-tags']['latest']
-                package_url = doc['versions'][latest]['dist']['tarball']
-                # package_url = f"https://registry.npmjs.org/{package_name}/-/{package_name}-{doc['dist-tags']['latest']}.tgz"
-                package_filename = f"{name}_package.tgz"
-                package_path = os.path.join(package_dir, package_filename)
-                response = requests.get(package_url)
-                if response.status_code == 200:
-                    with open(package_path, 'wb') as package_file:
-                        package_file.write(response.content)
-                    print("saved tar file")
-                    
-                    if os.path.getsize(package_path) > max_size:
-                        os.remove(package_path)
-                        print("tar too large, not saved")
+                if os.path.getsize(tarball_path) > MAX_SIZE:
+                    os.remove(tarball_path)
+                    print("Tarball too large, removed")
+                    if doc_path:
                         os.remove(doc_path)
-                        print("corresponding json not saved as well")
-                        saved = False
-            if saved:
-                return doc_path, package_path
+                        print("Corresponding JSON removed as well")
+                        doc_path = None
+                    saved = False
+                    tarball_path = None
+                    
+            return doc_path, tarball_path
     return None, None
 
-def compress_files(package_name, revision_id, doc_path, package_path):
-    package_dir = create_directory_structure(package_name)
+# Function to compress the downloaded JSON and tarball into a zip file and store it in remote directory
+def compress_files(raw_package_name, package_name, revision_id, doc_path, tarball_path):
+    package_dir = create_directory_structure(raw_package_name)
     compressed_filename = f"{package_name}_{revision_id}.zip"
     zip_path = os.path.join(package_dir, compressed_filename)
+    
     with zipfile.ZipFile(zip_path, 'w') as zip_file:
         if doc_path:
             zip_file.write(doc_path, os.path.basename(doc_path))
-            os.remove(doc_path)  # Remove the individual JSON file after compression
-        if package_path:
-            zip_file.write(package_path, os.path.basename(package_path))
-            os.remove(package_path)  # Remove the individual tar file after compression
+            os.remove(doc_path)  # Remove the individual JSON file from local (temp) directory after compression
+        if tarball_path:
+            zip_file.write(tarball_path, os.path.basename(tarball_path))
+            os.remove(tarball_path)  # Remove the individual tar file from local (temp) directory after compression
+    print("Compressed zip saved in remote")
+    return zip_path
 
-def store_change_details(change, db):
+# Function to save the processed change details in our own database
+def store_change_details(change, db, zip_path):
     # Store the important details regarding the change in the local CouchDB database.
-    # Replace the following with your code to interact with the local CouchDB.
 
     package_name = change['id']
     change_seq_id = change['seq']
@@ -151,23 +148,26 @@ def store_change_details(change, db):
         'package_revision_id': package_revision_id,
         'package_latest_version': package_latest_version,
         'package_versions_count': package_versions_count,
-        'package_latest_change_time': package_latest_change_time
+        'package_latest_change_time': package_latest_change_time,
+        'change_save_path': zip_path
     }
     db.save(data)
+    print("Change record added to database")
 
+# Main function that reads changes from the NPM Registry and processes them by downloading
+# corresponding files and making an entry for the change into our database
 def stream_npm_updates():
+    # access the changes API from Replicate (Public DB Replica for NPM Registry)
     url = 'https://replicate.npmjs.com/_changes?include_docs=true&feed=continuous&heartbeat=10000&style=all_docs&conflicts=true&since=25318031' #&limit=20'
-
     response = requests.get(url, stream=True)
-
     if response.status_code != 200:
         print(f'Error connecting to the CouchDB stream: {response.status_code}')
         return
 
-    # Create or connect to the database
-    db = create_or_connect_db(server, database_name)
+    # Create or connect to our database
+    db = create_or_connect_db(server, DATABASE_NAME)
 
-    # print(response)
+    # counter for number of changes read from the stream
     i = 0
 
     for line in response.iter_lines():
@@ -176,22 +176,20 @@ def stream_npm_updates():
         # print(line)
         if line:
             change = json.loads(line)
-            doc_path, package_path = download_document_and_package(change)
+            raw_package_name = change['id']
+            print("Change sequence ID: ", change['seq'])
+            print("Raw package name: ", raw_package_name)
+            if "/" in raw_package_name:
+                segments = raw_package_name.split("/")
+                package_name = segments[-1]
+            else:
+                package_name = raw_package_name
+            
+            doc_path, tarball_path = download_document_and_package(change, package_name)
+            
             if doc_path:
-                package_name = change['id']
-                if "/" in package_name:
-                    segments = package_name.split("/")
-                    name = segments[-1]
-                else:
-                    name = package_name
-                compress_files(name, change['doc']['_rev'], doc_path, package_path)
-                # Get the package name to store the change details
-                package_name = change.get('doc', {}).get('name')
-                if package_name:
-                    # Update the change count for the package and get the updated count
-                    # change_count = update_change_count(package_name)
-                    # Store important details regarding the change in the local CouchDB
-                    store_change_details(change, db)
+                zip_path = compress_files(raw_package_name, package_name, change['doc']['_rev'], doc_path, tarball_path)
+                store_change_details(change, db, zip_path)
         break
 
 stream_npm_updates()
